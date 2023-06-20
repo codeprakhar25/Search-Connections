1b27cbe06369178a79c35c5659dab435a69919e2
codeprakhar25 <prakharkhatri123@gmail.com>
2023-06-20 20:37:53 +0530

    feat: Search added

--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -14,26 +14,35 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 interface MyComponentProps {
   text1: string;
   text2: any;
+  function: Function,
 }
 
 const SearchBar: React.FC<MyComponentProps>  = (props) => {
     // const [ searchText, setSearchText] = useState('');
   
-    const handleSearch = () => {
+    const handleSearch = async () => {
       console.log('Search:', props.text1);
+      props.function();
     };
   
     const handleClearSearch = () => {
       props.text2('');
     };
-  
     return (
       <View style={{ flexDirection: 'row', alignItems: 'center', padding: 10 }}>
         <TextInput
           style={{ flex: 1, height: 40, borderWidth: 1, borderRadius: 5, paddingHorizontal: 10 }}
           placeholder="Search"
           value={props.text1}
-          onChangeText={text => props.text2(text)}
+          onChangeText={text => {
+            props.text2(text);
+          }
+        }
+        onChange={()=>{
+             props.function;
+             
+           }
+          }
         />
         {props.text1 !== '' && (
           <TouchableOpacity onPress={handleClearSearch} style={{ marginLeft: 10 }}>
--- src/features/Home.tsx
+++ src/features/Home.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable prettier/prettier */
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react-native/no-inline-styles */
 /* eslint-disable space-infix-ops */
 /* eslint-disable no-trailing-spaces */
@@ -6,67 +7,83 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable semi */
 
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Text,View} from 'react-native'
 import Header from '../app/components/Header'
 import ScreenWrapper from '../app/components/ScreenWrapper'
 import SearchBar from '../app/components/SearchBar'
-import { useQuery } from 'react-query';
+import { useMutation, useQuery } from 'react-query';
 import Loading from '../app/components/Loading'
 import { Image } from 'react-native'
 import { Picker } from '@react-native-picker/picker'
 
 const Home = () => {
   const [searchvalue, setSearchValue]=useState('')
+
   const fetchData = async () => {
     const response = await fetch('https://run.mocky.io/v3/0bff210c-7fc8-4964-a555-8d93de3d5f17');
-    const data = await response.json();
+   const  fetdata = await response.json();
+        const data = fetdata.filter((item:any) => {
+            // Perform search based on item properties
+            // For example, if item has a 'name' property:
+            return item.firstname.toLowerCase().includes(searchvalue.toLowerCase());
+        });
     console.log(data)
-    return data;
-  };
 
-  const { isLoading, error, data } = useQuery('myData', fetchData);
+    return data;
+};
+
+
+useEffect(() => {
+    fetchData();
+  
+    console.log('myState has changed:', searchvalue);
+  }, [searchvalue]); 
+
+const {isSuccess, isLoading, error,data} = useQuery(['search', searchvalue], fetchData);
 
   if (isLoading) {
     return <Text><Loading/></Text>;
-  }
+}
 
-  if (error) {
+else if (error) {
     return <Text>Error: {error.message}</Text>;
-  }
+}
+
 
 
 
   return (
     <>
     <Header text='Your Connections'/>
-    <SearchBar text1={searchvalue} text2={setSearchValue}/>
+    <SearchBar text1={searchvalue} text2={setSearchValue} function={fetchData}/>
    <View  style={{display:'flex' , flexDirection:'row',marginLeft: 20,width:50, justifyContent:'space-between'}}>
         <Image 
         source={require('../app/assets/sort.png')}
         />
         <Text>Sort</Text>
         </View>
-        {/* <Picker
+        <Picker
 //   selectedValue={sortOption}
 //   onValueChange={handleSortOptionChange}
 >
   <Picker.Item label="Sort by: None" value="" />
   <Picker.Item label="Sort by: Ascending" value="asc" />
   <Picker.Item label="Sort by: Descending" value="desc" />
-</Picker> */}
+</Picker>
     {data && (
-        <View>
-          {data.map((item: any) => (
-            <>
-            <Text key={item.id}>{item.age}</Text>
-            <Image 
-            source={item.picture}
-            />
-            </>
-          ))}
-        </View>
-      )}
+  <View>
+  {data.map((item: any) => (
+    <>
+    <Text >{item.age}</Text>
+    <Image 
+    source={item.picture}
+    />
+    </>
+  ))}
+</View>
+      )
+  }
     </>
   )
 }
